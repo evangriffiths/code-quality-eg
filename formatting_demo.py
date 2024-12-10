f03b9ffee5f5fae6c81cc82e4402650a81c2a3ab
evangriffiths <egriffiths93@gmail.com>
2024-12-10 20:27:22 +0000

    fix

--- formatting_demo.py
+++ formatting_demo.py
@@ -1,14 +1,14 @@
-import json # An unused import
+import pprint
+import json
 
 import os
 
-import pprint
 
 def  a_weird_function(
     x: list[int]) ->int:
             y = x[0]
             z = x[2]
-            a = 1 # an unused variable
+            a = 1
             return 2    * (y + z    )
 
 pprint.pprint(a_weird_function([1, 2, 3]))
